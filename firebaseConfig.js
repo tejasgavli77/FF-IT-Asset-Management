3e977b9ac5a65f70adeb2dcea23193a819cb2f99
tejasgavli <tejas.gawali@finalfunnel.com>
2025-04-25 00:45:57 +0530

    firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,6 @@
-// firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getFirestore, collection } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
-// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAspahfUUGnBzh0mh6U53evGQzWQP956xQ",
   authDomain: "ffassetmanager.firebaseapp.com",
@@ -13,11 +11,8 @@ const firebaseConfig = {
   appId: "1:803858971008:web:72d69ddce6cbc85010a965"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Firestore
 const db = getFirestore(app);
 const assetsCollection = collection(db, "assets");
 
-export { db, assetsCollection };
+export { db, assetsCollection, addDoc };
