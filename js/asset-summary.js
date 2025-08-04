94f24737fb347a6bafda22fa65b66190d4c289e4
tejasgavli <tejas.gawali@finalfunnel.com>
2025-08-05 00:14:59 +0530

    Update asset-summary.js

--- js/asset-summary.js
+++ js/asset-summary.js
@@ -30,11 +30,20 @@ function populateTypeDropdown(assets) {
   const types = [...new Set(assets.map(asset => asset.type))];
   types.sort();
 
+  // Clear previous options except "All Types"
+  typeFilter.innerHTML = `<option value="all">All Types</option>`;
+
   types.forEach(type => {
     const option = document.createElement("option");
     option.value = type;
     option.textContent = type;
     typeFilter.appendChild(option);
+  });
+
+  const choices = new Choices("#typeFilter", {
+    searchEnabled: true,
+    itemSelectText: "",
+    shouldSort: false,
   });
 
   typeFilter.addEventListener("change", () => {
