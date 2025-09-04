b9ff1d552de15f81cf0c476b0d371e37ec0639e3
tejasgavli77 <tejas.gawali7@gmail.com>
2025-09-04 22:47:07 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,22 @@
 // firebaseConfig.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAspahfUUGnBzh0mh6U53evGQzWQP956xQ",
-  authDomain: "ffassetmanager.firebaseapp.com",
-  databaseURL: "https://ffassetmanager-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ffassetmanager",
-  storageBucket: "ffassetmanager.firebasestorage.app",
-  messagingSenderId: "803858971008",
-  appId: "1:803858971008:web:72d69ddce6cbc85010a965"
+  apiKey: "AIzaSyAj5x6hsWKmy3l0I2j8npj4u2woUXkIEp4",
+  authDomain: "asset-inventory-f6b04.firebaseapp.com",
+  projectId: "asset-inventory-f6b04",
+  storageBucket: "asset-inventory-f6b04.appspot.com",
+  messagingSenderId: "988574661528",
+  appId: "1:988574661528:web:ec0808edf25cad1ef5c9ed",
+  measurementId: "G-JEN1YSKN2L"
 };
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const assetsCollection = collection(db, 'assets');
 
-export { db, assetsCollection, getDocs };
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
